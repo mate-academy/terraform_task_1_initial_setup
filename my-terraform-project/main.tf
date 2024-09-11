53b3efaf282292afad230e5bcc437e9c60c5f6f2
Serveladus <serveladik@gmail.com>
2024-09-11 15:52:49 +0300

    Solution2

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -18,8 +18,8 @@ resource "azurerm_resource_group" "mate-terraform-rg" {
 
 resource "azurerm_storage_account" "mate-terraform-sa" {
   name                     = "mateterraformstorage"
-  resource_group_name      = azurerm_resource_group.mate-terraform-sa.name
-  location                 = azurerm_resource_group.mate-terraform-sa.location
+  resource_group_name      = azurerm_resource_group.mate-terraform-rg.name
+  location                 = azurerm_resource_group.mate-terraform-rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
