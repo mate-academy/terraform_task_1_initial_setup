a8fce9a92c8213695cb130cfc67aa41b6233f117
Alex13-th <aleks.shaparenko@gmail.com>
2025-07-14 13:28:40 +0200

    Solution

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "3.105.0"
+      version = "4.36.0"
     }
   }
 }
@@ -12,7 +12,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "my-example-terraform-resources"
   location = "West Europe"
 }
 
