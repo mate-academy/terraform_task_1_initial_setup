e8e1ec70591504b14d532c6ee24cca2841732a11
Yevgene-DP <ebalalay@gmail.com>
2025-09-11 19:23:33 +0300

    Update resource group name and add student tag

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -1,25 +1,21 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.105.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "my-unique-rg-name-12345"  # Змініть на унікальну назву
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"
+  name                     = "myuniquestorage12345"  # Змініть на унікальну назву
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+  tags = {
+    environment = "test"
+    student     = "Yevgene-DP"  # Додайте ваш username
+  }
 }
