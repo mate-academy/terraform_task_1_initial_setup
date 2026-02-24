d4eec645d0b318e7a1034b5e65d49b46d9938525
Gosxa <ihorkondrashov@gmail.com>
2026-02-24 21:45:07 +0200

    terraform init

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -12,14 +12,14 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "my-example-resources"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "example" {
   name                     = "examplestorageacc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.myexample.name
+  location                 = azurerm_resource_group.myexample.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
