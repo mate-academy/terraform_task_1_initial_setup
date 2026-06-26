01198fbc5a0f2570007f4e9ad100b7b27cf78638
Nikita Ivanichenko <importantthings1482@gmail.com>
2026-06-26 18:37:00 +0300

    fix

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -11,13 +11,13 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "rg" {
+  name     = "task-resources"
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"
+resource "azurerm_storage_account" "st" {
+  name                     = "st-resources"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
