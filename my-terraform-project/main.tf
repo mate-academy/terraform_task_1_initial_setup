860378edc4b91a5a6b95f58d2f96196085a9b482
dianahorban <dianahorban477@gmail.com>
2026-07-17 13:57:01 +0200

    the solution

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_resource_group" "example" {
 
 
 resource "azurerm_storage_account" "example" {
-  name                     = "diana-resource-group-pomidorchickichky"
+  name                     = "p-pomidorchickichky"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
