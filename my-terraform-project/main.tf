00697b7cb95e78c360e61c0a210a1720c9bc38b1
Anastasiia <anastasiia.chikrizova@dashdevs.com>
2026-08-03 20:01:48 +0300

    changed main.tf

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -12,8 +12,8 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+  name     = "mate-resources"
+  location = "Denmark East"
 }
 
 resource "azurerm_storage_account" "example" {
