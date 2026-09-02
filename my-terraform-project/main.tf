0bc0d6ade556a4155cb39cad31c6223611f26ba8
Vitalii <stalchenkovitaliy@gmail.com>
2026-09-02 16:01:30 +0400

    initialization terraform

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -12,8 +12,8 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+  name     = "mateapp-development-rg"
+  location = "Poland Central"
 }
 
 resource "azurerm_storage_account" "example" {
